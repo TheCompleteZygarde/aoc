fc1752d4e491188298706bdc27b1fc0c44c65717
Ola Eklund <ola.eklund@live.se>
2025-12-20 22:33:03 +0100

    Day 10 finished

--- 2025/10.py
+++ 2025/10.py
@@ -1,4 +1,6 @@
 from itertools import combinations
+import numpy
+import scipy.optimize as op
 with open("input_day10.txt", "r") as file:
     data = file.readlines()
 
@@ -20,3 +22,19 @@ for line in data:
     buttons = [list(map(int, val[1:-1].split(','))) for val in line[1:-1]]
     pw += loop(lights, buttons)
 print(pw)
+
+
+pw2 = 0
+for line in data:
+    line = line.strip().split()
+    targets = numpy.array(list(map(int, line[-1][1:-1].split(','))))
+    buttons = [set(map(int, val[1:-1].split(','))) for val in line[1:-1]]
+    nbuttons = numpy.array([[1 if i in buttons[n] else 0 for i in range(len(targets))] for n in range(len(buttons))])
+    nbuttons = numpy.transpose(nbuttons)
+    solution = op.milp(
+        numpy.ones(len(nbuttons[0])), 
+        integrality=numpy.ones(nbuttons.shape[1], dtype=int), 
+        constraints=op.LinearConstraint(nbuttons, targets, targets), 
+        bounds=op.Bounds(0, numpy.inf))
+    pw2 += int(solution.fun)
+print(pw2)
